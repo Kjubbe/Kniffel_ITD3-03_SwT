ca2c28e3274353022e9d1e0163f99976fa5a4928
Kjell Treder <kjell.treder@gmail.com>
2020-11-02 14:50:15 +0100

    added testing

---
+++ src/test/MainTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * testing class for testing main
+ * -> use seeds for testing
+ * 
+ * @author Kjell Treder
+ */
+
+public class MainTest {
+
+    /**
+     * setup before each method
+     */
+    @Before
+    public void setup() {
+        // setup goes here
+    }
+
+    /**
+     * test
+     */
+    @Test
+    public void test() {
+        // testing goes here
+    }
+}
