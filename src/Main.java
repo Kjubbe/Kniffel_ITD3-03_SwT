7bbc7c7d6912689c85a776a2337c6416241f24b6
simeonmurzin <simeon.murzin@shinefour.de>
2020-11-07 19:44:57 +0100

    Implemented primitive StartScreenGUI

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import view.StartScreenGUI;
 /**
  * Main
  * 
@@ -15,5 +16,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World"); // :)
 		// TODO
+		StartScreenGUI start = new StartScreenGUI();
 	}
 }
